22c99d5b61b15d884d5643b7dfebf8deed00ef8d
Alicia-cmd <61874578+Alicia-cmd@users.noreply.github.com>
2022-03-29 15:38:13 +0200

    Update Election.sol
    Mise à jour du code

--- Election.sol
+++ Election.sol
@@ -9,14 +9,14 @@ contract President is Ownable {
 
 using SafeMath for uint256;
 
-    // Objet liste de tous les candidats
+    // Objet liste de tous les candidats qui existe
     struct Candidat {
         uint id;
         address ethadress;
         string name;
     }
     
-    // Objet liste des candidats selectionnés (500 signatures) 
+    // Objet liste des candidats selectionnés (exemple: 500 signatures) 
     struct CandidatSelectionned {
         uint id;
         string name;
